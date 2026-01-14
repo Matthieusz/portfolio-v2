a0ec68149008cfb40c18497a8f5104791d75ede1
informati <46190977+Matthieusz@users.noreply.github.com>
2026-01-14 17:57:18 +0100

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
+import node from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
